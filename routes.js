33fda53fb23fa1a47dc681f0729f87dee1bc9b36
derek-alves <derek.alves@fatec.sp.gov.br>
2020-04-10 14:47:58 -0300

    Front-end: Create Instructors

--- routes.js
+++ routes.js
@@ -8,10 +8,17 @@ routes.get('/instructors', function(req,res){
     return res.render('instructors/index')
 })
 
+routes.get('/instructors/create', function(req,res){
+    return res.render('instructors/create')
+})
+
 routes.get('/members', function(req,res){
     return res.send('/members')
 })
 
 
+routes.post("/instructors", function(req,res){
+    return res.send("recebido")
+})
 
 module.exports = routes
